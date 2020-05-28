9204241ae3c79a2d77a83f3e1037f38d6fdefd14
Antonio Moreno Cantó <elton3t@gmail.com>
2020-05-28 17:51:14 +0200

    colores formu

--- javascript/formu.js
+++ javascript/formu.js
@@ -36,47 +36,56 @@ function validarFormu() {
   var formu = document.getElementById('miformulario');
   //var forChecked = document.getElementsByName('aceptar');
 
-  var email = document.getElementById('email').value;
+
   var edad = document.getElementById('edad').value;
   var txtArea = document.getElementById('txtArea');
   var f1 = document.getElementById('f1');
+  valor = document.getElementById("email").value;
+  
 
-  if (formu[0].value == "" || formu[0].value == null) {
-    document.getElementById("txtArea").style.backgroundColor = "red";
+  if (formu[0].value == "" || formu[0].value == null || formu[0].length < 100) {
+    alert("El comentario deber tener 100 caracteres.");
+    document.getElementById("txtArea").style.backgroundColor = "rgb(255, 0, 0, 0.3)";
+    document.getElementById("txtArea").focus();
   } else {
-    document.getElementById("txtArea").style.backgroundColor = "white";
+    document.getElementById("txtArea").style.backgroundColor = "lightblue";
     if (formu[1].value == "" || formu[1].value == null) {
-      document.getElementById("f1").style.backgroundColor = "red";
+      document.getElementById("f1").style.backgroundColor = "rgb(255, 0, 0, 0.3)";
+      document.getElementById("f1").focus();
     } else {
-      document.getElementById("f1").style.backgroundColor = "white";
+      document.getElementById("f1").style.backgroundColor = "lightblue";
       if (formu[2].value == "" || formu[2].value == null) {
-        document.getElementById("f2").style.backgroundColor = "red";
+        document.getElementById("f2").style.backgroundColor = "rgb(255, 0, 0, 0.3)";
+        document.getElementById("f2").focus();
       } else {
-        document.getElementById("f2").style.backgroundColor = "white";
-        if (formu[3].value == "" || formu[3].value == null) {
-          document.getElementById("email").style.backgroundColor = "red";
+        document.getElementById("f2").style.backgroundColor = "lightblue";
+        if (!(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(valor))) {
+          document.getElementById("email").style.backgroundColor = "rgb(255, 0, 0, 0.3)";
+          document.getElementById("email").focus();
         } else {
-          document.getElementById("email").style.backgroundColor = "white";
+          document.getElementById("email").style.backgroundColor = "lightblue";
           if (formu[4].value == "" || formu[4].value == null || isNaN(edad)) {
-            document.getElementById("edad").style.backgroundColor = "red";
+            document.getElementById("edad").style.backgroundColor = "rgb(255, 0, 0, 0.3)";
+            document.getElementById("edad").focus();
           } else {
-            document.getElementById("edad").style.backgroundColor = "white";
+            document.getElementById("edad").style.backgroundColor = "lightblue";
             if (formu[5].checked == false || formu[5].checked == null) {
-              document.getElementById("terminos").style.color = "red";
+              document.getElementById("terminos").style.color = "rgb(255, 0, 0, 0.3)";
+              document.getElementById("terminos").focus();
             } else {
-              document.getElementById("terminos").style.color = "white";
+              document.getElementById("terminos").style.color = "lightblue";
               alert("Formulario rellenado");
 
               /* Vamos a crear una seccion con el comentario del cliente */
 
-              
+
               // Crear nodo de tipo Element
               var salto = '<br>';
 
 
               // Crear nodo de tipo Text
               var op = document.getElementById("txtArea").value + (document.getElementById("txtArea").innerHTML = "<br/>") + document.getElementById('f1').value + "<br/>" + document.getElementById('email').value + "<br/>";
-              
+
               var contenido = document.createTextNode(op);
 
               //section.appendChild(contenido);
@@ -84,8 +93,8 @@ function validarFormu() {
               zona = document.getElementById('firmas');
 
               zona.appendChild(contenido);
-              
-              
+
+
 
               /*
 
@@ -101,13 +110,18 @@ function validarFormu() {
 
               txtArea.value = "";
               f1.value = "";
+              f1.style.backgroundColor = "white";
               var f2 = document.getElementById('f2');
               f2.value = "";
+              f2.style.backgroundColor = "white";
               var email2 = document.getElementById('email');
               email2.value = "";
+              email2.style.backgroundColor = "white";
               var edad2 = document.getElementById('edad');
               edad2.value = "";
+              edad2.style.backgroundColor = "white";
               document.getElementById("terminos").checked = 0;
+              document.getElementById("txtArea").style.backgroundColor = "white";
 
 
 
